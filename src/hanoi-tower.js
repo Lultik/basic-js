538ce1d94ea33df6a2104179a73a3068919fb0a1
lultik <mihwil27021@gmail.com>
2020-02-24 22:13:14 +0300

    feat: resolve recursive depth

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,6 +2,6 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     function calculateTurns(disksNumber) {
         return disksNumber === 1 ? 1 : 2*calculateTurns(disksNumber-1) + 1
     }
-    let turns = calculateTurns(disksNumber)
+    let turns = calculateTurns(disksNumber);
     return {turns: turns, seconds: turns / (turnsSpeed/3600)}
-}
+};
