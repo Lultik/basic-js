2aab1b76454a3c74611d5a6d20993fdcb8a0bead
lultik <mihwil27021@gmail.com>
2020-02-25 23:36:51 +0300

    feat: resolve vigenere cipher encryption

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,28 @@
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        let count = 0;
+        key = key.repeat(message.length/key.length + 1 ).toLowerCase()
+        return message.toLowerCase().split('').map((item,index) => {
+            if(/[A-Za-z]/.test(item)) {
+                let letterNumber = alphabet.indexOf(item)+alphabet.indexOf(key[count]);
+                count++;
+                return alphabet[letterNumber%26]
+            }else return item
+        }).join('').toUpperCase()
+
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(code, key) {
+        let count = 0;
+        key = key.repeat(code.length/key.length + 1 ).toLowerCase()
+        return code.toLowerCase().split('').map((item,index) => {
+            if(/[A-Za-z]/.test(item)) {
+                let letterNumber = 26 + alphabet.indexOf(item)-alphabet.indexOf(key[count]);
+                count++;
+                return alphabet[letterNumber%26]
+            }else return item
+        }).join('').toUpperCase()
     }
 }
 
