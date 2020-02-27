0dda2557f4bfd10a8ccf6f922b3887ecaebcb44f
lultik <mihwil27021@gmail.com>
2020-02-28 00:29:20 +0300

    feat: resolve

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,9 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options ) {
+    if(!options.separator) options.separator = '+'
+    if(!options.additionSeparator) options.additionSeparator = '|'
+    if(options.addition === null) options.addition = 'null'
+    let addition = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator)
+    let general = new Array(options.repeatTimes).fill(str+addition).join(options.separator)
+    return general;
 };
   
