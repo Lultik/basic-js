29653c8727a3a5d55a906c44132bbb60ae29de18
Lultik <mihwil27021@gmail.com>
2021-02-03 15:56:14 +0300

    feat: complete count-cat task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,12 +1,7 @@
 module.exports = function countCats(arr) {
-  let catCount = 0;
-  for(let i = 0; i < arr.length; i++){
-    for(let j = 0; j < arr[i].length; j++){
-      if(typeof arr[i][j] === 'string') {
-        if(arr[i][j].match(/\^/g) && !!arr[i][j].match(/^\^{2}$/g)){
-          catCount++
-        } else catCount += 0
-      }else catCount += 0
-    }}
-  return catCount
+  let countOfCats = 0
+     arr.flat().map((item) => {
+    item === '^^' ? countOfCats++ : 0;
+  })
+  return countOfCats
 }
