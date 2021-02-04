ee02f38b490bc7afa94d5eb24dd1d59b57502424
Lultik <mihwil27021@gmail.com>
2021-02-04 13:07:06 +0300

    feat: complete task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = [];
+
+  if(!Array.isArray(members)) return false
+
+  members.map((item) => {
+    if(typeof item === 'string') {
+      dreamTeam.push([...item.match(/[A-Za-z]/)].join('').toUpperCase())
+    }
+  })
+  return dreamTeam.sort().join('');
 };
