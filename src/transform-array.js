a4a42983362f8aa7ce4d09fd14ab1440e582d01a
Lultik <mihwil27021@gmail.com>
2021-02-06 17:24:09 +0300

    feat: complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+   if (!Array.isArray(arr)) throw new Error()
+   let transformedArray =[]
+   for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+         case '--discard-next':
+            transformedArray.push('discard');
+            ++i;
+            break;
+         case '--discard-prev':
+            if(i) transformedArray.splice(-1,1).push('discard');
+            break;
+         case '--double-next':
+            if(i !== arr.length - 1) transformedArray.push(arr[i + 1], )
+            break;
+         case '--double-prev':
+            if(i) transformedArray.push(transformedArray[transformedArray.length-1] )
+            break;
+         default:
+            transformedArray.push(arr[i])
+            break;
+      }
+   }
+ return transformedArray.filter(item => item !== 'discard')
 };
